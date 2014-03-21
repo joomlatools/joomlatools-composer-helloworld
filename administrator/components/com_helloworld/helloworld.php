b8ed651af009625c07d8877063ba78d5335260a8
Ercan Ozkaya <ercan@timble.net>
2014-03-21 13:47:49 +0200

    Add an error view to generate a fatal error on demand

--- administrator/components/com_helloworld/helloworld.php
+++ administrator/components/com_helloworld/helloworld.php
@@ -1,1 +1,10 @@
-Hello World!
+<?php
+
+$view = JFactory::getApplication()->input->getCmd('view', 'default');
+
+if ($view === 'error') {
+    // Generate a fatal error
+    i_am_so_undefined();
+} else {
+    echo 'Hello World!';
+}
