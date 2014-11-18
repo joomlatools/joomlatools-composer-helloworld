ed7754d9cc6f39beaa2e5cea0c7a13bd45e34f60
Steven Rombauts <steven@timble.net>
2014-11-18 10:19:53 +0100

    Add license info

--- site/components/com_helloworld/helloworld.php
+++ site/components/com_helloworld/helloworld.php
@@ -1,4 +1,11 @@
 <?php
+/**
+ * Hello World example component
+ *
+ * @copyright   Copyright (C) 2011 - 2014 Timble CVBA. (http://www.timble.net)
+ * @license     GNU GPLv3 <http://www.gnu.org/licenses/gpl.html>
+ * @link        https://github.com/joomlatools/joomla-com_helloworld for the canonical source repository
+ */
 
 $view = JFactory::getApplication()->input->getCmd('view', 'default');
 
